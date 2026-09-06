a99f03faed5c651da648e0c9ecb2baaf25e24d98
rotemamt <rotemamt@gmail.com>
2026-09-06 23:59:17 +0300

    Fix static files (WhiteNoise) + GitHub OIDC immutable subject claims

--- infra/github-actions-oidc.tf
+++ infra/github-actions-oidc.tf
@@ -14,8 +14,7 @@ resource "aws_iam_role" "github_actions_deploy" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:rotemamt/statuspage-terraform:*"
-        }
+          "token.actions.githubusercontent.com:sub" = "repo:rotemamt/statuspage-terraform:*"        }
       }
     }]
   })
@@ -24,6 +23,58 @@ resource "aws_iam_role" "github_actions_deploy" {
 resource "aws_iam_role_policy" "github_actions_deploy" {
   name = "GitHubActionsDeployPolicy"
   role = aws_iam_role.github_actions_deploy.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["ecr:GetAuthorizationToken"]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["ecr:BatchCheckLayerAvailability", "ecr:PutImage", "ecr:InitiateLayerUpload", "ecr:UploadLayerPart", "ecr:CompleteLayerUpload"]
+        Resource = "arn:aws:ecr:us-east-1:992382545251:repository/ro-ro-statuspage-dev-repo"
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["eks:DescribeCluster"]
+        Resource = "arn:aws:eks:us-east-1:992382545251:cluster/ro-ro-statuspage-cluster"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "github_actions_deploy_v2" {
+  name = "GitHubActionsDeployRoleV2"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = "arn:aws:iam::992382545251:oidc-provider/token.actions.githubusercontent.com"
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+        }
+        StringLike = {
+          "token.actions.githubusercontent.com:sub" = [
+            "repo:rotemamt/statuspage-terraform:*",
+            "repo:rotemamt@229573277/statuspage-terraform@1354911446:*"
+          ]
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_deploy_v2" {
+  name = "GitHubActionsDeployPolicy"
+  role = aws_iam_role.github_actions_deploy_v2.id
 
   policy = jsonencode({
     Version = "2012-10-17"
